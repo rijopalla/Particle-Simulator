540992bb3df40ac2c1e84a685a28241a4a1affac
rijopalla <82391730+rijopalla@users.noreply.github.com>
2024-05-27 18:28:52 +0800

    initial push

---
+++ Particle.java
@@ -0,0 +1,21 @@
+
+public class Particle {
+    public float x, y;
+    public float angle;
+    public float velocity;
+
+    public float getAngle() {
+        return this.angle;
+    }
+
+    public float getVelocity() {
+        return this.velocity;
+    }
+
+    // public float updatePosition() {
+
+    // }
+
+    
+}
+
